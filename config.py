44f58b9590f7c073d4ed0038f1b977596e0c675b
Alex Hixson <hixsonaj@hotmail.com>
2024-11-30 22:46:31 -0800

    Added Goated.py

--- config.py
+++ config.py
@@ -1,11 +1,20 @@
 from pypokerengine.api.game import setup_config, start_poker
 from default_agents import FishPlayer, FoldMan, RandomPlayer, HonestPlayer
 from console_player import ConsolePlayer
+from goated import Goated
 
 config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
+
+#Default Agents
 config.register_player(name="Fish Player", algorithm=FishPlayer())              #Call machine
 config.register_player(name="Fold Man", algorithm=FoldMan())                    #Extremely tight
 config.register_player(name="Random Player", algorithm=RandomPlayer())          #Possibly intoxicated
 config.register_player(name="Honest Player", algorithm=HonestPlayer())          #Dudes trying his best
-config.register_player(name="Console Player", algorithm=ConsolePlayer())        #I'm taking their money
+
+#User Input
+# config.register_player(name="Console Player", algorithm=ConsolePlayer())        #I'm taking their money
+
+#Goated Poker AI
+config.register_player(name="Goated Poker-AI", algorithm=Goated())              #Goated
+
 game_result = start_poker(config, verbose=1)
